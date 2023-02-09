b18daace25630d280ec85b188dfc50f9ac0edcf9
Dwi Ngadiyanto <dwingadiyanto879@gmail.com>
2023-02-09 21:45:32 +0700

    Tugas 2 Javascript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas2.js
@@ -0,0 +1,34 @@
+ var bulat = 20;
+var desimal = 30.5;
+var negatif = -10;
+var a = 5 * 9;
+var b = 8 + 2;
+console.log(bulat, desimal, negatif, a, b);
+
+var huruf1 = "Ini adalah string sederhana";
+var huruf2 = "Dia berkata: \"I'll be back\"";
+var huruf3 = "Kalimat ini akan akan pindah ke: \n baris baru";
+var huruf4 = "Variabel akan otomatis ditampilkan: \$huruf1 dan \$huruf2";
+var huruf5 = 'tunggal';
+
+console.log(huruf1);
+console.log(huruf2);
+console.log(huruf3);
+console.log(huruf4);
+console.log(huruf5);
+
+var tinggi = false
+var cm = 165;
+
+if(cm>170)
+tinggi = true;
+else
+tinggi = false;
+
+var fal = false;
+var space = "  ";
+var space1 = "";
+
+ console.log(tinggi);
+ console.log(fal);
+ console.log(space);
